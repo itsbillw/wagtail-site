bc3695325bae47616e4b50e33aa002bf33b59972
itsbillw <itsbillw@gmail.com>
2023-07-11 01:04:37 +0200

    First page change

--- mysite/mysite/settings/dev.py
+++ mysite/mysite/settings/dev.py
@@ -1,13 +1,18 @@
+import os
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-q2n445zg1x4g3-rn)0$y_y3@9dgr%2-)*l13vzl5ef7gd4i&y("
+try:
+    SECRET_KEY = os.environ["SECRET_KEY"]
+except KeyError as e:
+    raise RuntimeError("Could not find a SECRET_KEY in environment") from e
+
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [".itsbillw.xyz", "*"]
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
